00fe6d3d3b7a3000a2a7b971b31d1bf94f62d575
Md nayeem Hossain <mdnayeemhossain@Mds-MacBook-Pro.local>
2020-07-17 13:03:19 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,23 @@
+//feet to mile converter
+function feetToMile(n){
+
+}
+
+//wood calculator for furniture
+function woodCalculator(){
+
+}
+
+
+//calucate brick 
+function brickCalculator(){
+
+}
+
+
+
+//find friend with smallest name 
+function tinyFriend(){
+    
+}
+
